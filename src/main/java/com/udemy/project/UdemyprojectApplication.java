d9527960d8754748a5428ea5ad50b32440514240
well.raul <well.raulg@gmail.com>
2018-06-02 19:25:08 -0300

    Criando Operação de Instanciação

--- src/main/java/com/udemy/project/UdemyprojectApplication.java
+++ src/main/java/com/udemy/project/UdemyprojectApplication.java
@@ -1,12 +1,32 @@
 package com.udemy.project;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class UdemyprojectApplication {
+import com.udemy.project.domain.Categoria;
+import com.udemy.project.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class UdemyprojectApplication implements CommandLineRunner {
+
+	@Autowired 
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
