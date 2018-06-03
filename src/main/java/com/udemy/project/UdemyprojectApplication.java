07f358c252ca809650be882696c7bff122aabb32
well.raul <well.raulg@gmail.com>
2018-06-02 21:22:24 -0300

    Criação Repository e domain Produto mais associaçao categoria x produtos

--- src/main/java/com/udemy/project/UdemyprojectApplication.java
+++ src/main/java/com/udemy/project/UdemyprojectApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.udemy.project.domain.Categoria;
+import com.udemy.project.domain.Produto;
 import com.udemy.project.repositories.CategoriaRepository;
+import com.udemy.project.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class UdemyprojectApplication implements CommandLineRunner {
 
 	@Autowired 
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyprojectApplication.class, args);
@@ -26,7 +31,18 @@ public class UdemyprojectApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "IMpressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 200.00);
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2 ,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
---
+++ src/main/java/com/udemy/project/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.udemy.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.udemy.project.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
